0ba2a84ca477015d67137b8fb494837f52a56980
Matheus Marinho <matheus4551@hotmail.com>
2024-03-17 15:33:29 -0300

    Import sources

---
+++ autogen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-backgrounds"
+REQUIRED_AUTOMAKE_VERSION=1.9
+
+
+(test -f $srcdir/configure.in \
+  && test -d $srcdir/abstract ) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+echo "Running glib-gettextize"
+glib-gettextize --copy --force
+
+echo "Running intltoolize"
+intltoolize --copy --force --automake
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
